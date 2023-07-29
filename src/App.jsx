c9dc066ad912e4a0baa9541ae574fcae89903fb0
7S-Coder <dylan.e33@hotmail.fr>
2023-07-29 20:14:44 +0200

    add data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Accueil from "./components/accueil";
+import Accueil from "./components/Accueil";
+import Navbar from "./components/Navbar/Navbar";
+import About from "./components/About";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Accueil />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </>
   );
